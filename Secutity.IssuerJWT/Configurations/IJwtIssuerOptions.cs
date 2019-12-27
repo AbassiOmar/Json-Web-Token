a7bba7b1968789be6d66edd1257c6a21097b09c3
Omar ABASSI <oabassi@assurone-group.com>
2019-12-27 16:52:07 +0100

    first commit créate server authentification JWT

---
+++ Secutity.IssuerJWT/Configurations/IJwtIssuerOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Secutity.IssuerJWT.Configurations
+{
+   public  interface IJwtIssuerOptions
+    {
+        String Issuer { get; }
+        String Audience { get; }
+        TimeSpan ValidFor { get; }
+        DateTime NotBefore { get; }
+        DateTime IssuedAt { get; }
+        DateTime Expires { get; }
+
+        Task<String> JtiGenerator();
+
+        SigningCredentials SigningCredentials { get; }
+    }
+}
